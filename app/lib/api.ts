b756c128eb6d56fa1b1ba0111ae007bc7c32644c
bluecollarsoapbox <bluecollarsoapbox@gmail.com>
2025-09-03 03:58:53 -0400

    updated api

--- app/lib/api.ts
+++ app/lib/api.ts
@@ -1,4 +1,6 @@
 // app/lib/api.ts
-export const API_URL = "https://soapbox-server.onrender.com";
+export const API_URL =
+  process.env.EXPO_PUBLIC_API_URL || "https://soapbox-server.onrender.com";
+
 export const API_KEY = "99dnfneeekdegnrJJSN3JdenrsdnJ";
 export const AUTH_HEADER = { "x-soapbox-key": API_KEY };
