7ffbed1ba5eac9021ec36a06f3133ecc1ede3d66
bluecollarsoapbox <bluecollarsoapbox@gmail.com>
2025-09-15 04:12:38 -0400

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,9 @@
 module.exports = function (api) {
   api.cache(true);
   return {
+    // Expo SDK 50+ uses this preset (includes expo-router automatically)
     presets: ['babel-preset-expo'],
-    plugins: ['react-native-reanimated/plugin'],
+    // Reanimated moved its plugin here:
+    plugins: ['react-native-worklets/plugin'],
   };
 };
